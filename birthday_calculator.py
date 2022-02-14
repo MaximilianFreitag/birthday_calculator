bfc7f9c0fb89d87f252a227cfde284a98642fe79
Maximilian Freitag <46624616+MaximilianFreitag@users.noreply.github.com>
2022-02-14 17:44:16 +0100

    Update birthday_calculator.py

--- birthday_calculator.py
+++ birthday_calculator.py
@@ -15,38 +15,54 @@ st.set_page_config(
 
 
 
-#Display a GIF on top of the app
-st.markdown("![Alt Text](https://media.giphy.com/media/KbRBekX2PQtjVdlV8i/giphy.gif?cid=790b7611d454a209ea5ffa7a79b7330d4bc0a9f4ab0190be&rid=giphy.gif&ct=g)")
+
+col1, col2, col3, col4, col5 = st.columns([1,1,5,1,1])
 
 
-#Title of your app + one whitespace
-st.markdown("<h1 style='text-align: center; color: black;'>On which day were you born?</h1>", unsafe_allow_html=True)
-st.markdown("<h5 style='text-align: center; color: black;'>                 </h5>", unsafe_allow_html=True)
+with col1:
+        st.write("")
+
+with col5:
+        st.write("")        
 
 
-#Take input of the user
-year = st.number_input('Year', min_value=1900, max_value=2021, step=1)
-month = st.number_input('Month', min_value=1, max_value=12, step=1)
-day = st.number_input('Day', min_value=1, max_value=31, step=1)
+with col3:
+    
 
 
-#i used these lines for additional whitespace between the result and the text boxes
-st.markdown("<h5 style='text-align: center; color: black;'>                 </h5>", unsafe_allow_html=True)
-st.markdown("<h5 style='text-align: center; color: black;'>                 </h5>", unsafe_allow_html=True)
-st.markdown("<h5 style='text-align: center; color: black;'>                 </h5>", unsafe_allow_html=True)
+
+        #Display a GIF on top of the app
+        st.markdown("![Alt Text](https://media.giphy.com/media/KbRBekX2PQtjVdlV8i/giphy.gif?cid=790b7611d454a209ea5ffa7a79b7330d4bc0a9f4ab0190be&rid=giphy.gif&ct=g)")
 
 
-d = datetime.date(year, month, day)
-
-st.write('Your were born on a ... ' + c.day_name[d.weekday()])
-
-#whitespace again
-st.markdown("<h3 style='text-align: center; color: black;'>                 </h5>", unsafe_allow_html=True)
-st.markdown("<h3 style='text-align: center; color: black;'>                 </h5>", unsafe_allow_html=True)
+        #Title of your app + one whitespace
+        st.markdown("<h1 style='text-align: center; color: black;'>On which day were you born?</h1>", unsafe_allow_html=True)
+        st.markdown("<h5 style='text-align: center; color: black;'>                 </h5>", unsafe_allow_html=True)
 
 
-#instagram link
-link = '[Just a tiny example app for my Instagram post](https://www.instagram.com/max_mnemo)'
-st.markdown(link, unsafe_allow_html=True)
+        #Take input of the user
+        year = st.number_input('Year', min_value=1900, max_value=2021, step=1)
+        month = st.number_input('Month', min_value=1, max_value=12, step=1)
+        day = st.number_input('Day', min_value=1, max_value=31, step=1)
+
+
+        #i used these lines for additional whitespace between the result and the text boxes
+        st.markdown("<h5 style='text-align: center; color: black;'>                 </h5>", unsafe_allow_html=True)
+        st.markdown("<h5 style='text-align: center; color: black;'>                 </h5>", unsafe_allow_html=True)
+        st.markdown("<h5 style='text-align: center; color: black;'>                 </h5>", unsafe_allow_html=True)
+
+
+        d = datetime.date(year, month, day)
+
+        st.write('Your were born on a ... ' + c.day_name[d.weekday()])
+
+        #whitespace again
+        st.markdown("<h3 style='text-align: center; color: black;'>                 </h5>", unsafe_allow_html=True)
+        st.markdown("<h3 style='text-align: center; color: black;'>                 </h5>", unsafe_allow_html=True)
+
+
+        #instagram link
+        link = '[Just a tiny example app for my Instagram post](https://www.instagram.com/max_mnemo)'
+        st.markdown(link, unsafe_allow_html=True)
 
 
